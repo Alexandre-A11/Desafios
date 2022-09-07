35a14c3b5a39be6464dcda9a04c321a3a9492b32
Alexandre Almeida <alee.wxa@gmail.com>
2022-09-07 09:56:36 -0300

    feat: add desafio simple-multiplication CodeWars

---
+++ CodeWars/8kyu/simple-multiplication/SimpleMultiplication.php
@@ -0,0 +1,8 @@
+<?php
+
+class SimpleMultiplication {
+    function  simpleMultiplication($number) {
+        // your code goes here
+        return $number % 2 ? $number * 9 : $number * 8;
+      }
+}
